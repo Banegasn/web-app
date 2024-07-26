07dabf74eefd025102385518c013108d718e7f3d
banegasn <nicobanegas.sc@gmail.com>
2024-07-27 01:09:32 +0200

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection
+  provideExperimentalZonelessChangeDetection
 } from '@angular/core';
 import {
   provideRouter
@@ -17,80 +16,15 @@ import {
   provideFirebaseApp
 } from '@angular/fire/app';
 import {
-  getAuth,
-  provideAuth
-} from '@angular/fire/auth';
-import {
-  getAnalytics,
-  provideAnalytics,
-  ScreenTrackingService,
-  UserTrackingService
-} from '@angular/fire/analytics';
-import {
-  initializeAppCheck,
-  ReCaptchaEnterpriseProvider,
-  provideAppCheck,
-  ReCaptchaV3Provider
-} from '@angular/fire/app-check';
-import {
-  getFirestore,
-  provideFirestore
-} from '@angular/fire/firestore';
-import {
-  getDatabase,
-  provideDatabase
-} from '@angular/fire/database';
-import {
-  getFunctions,
-  provideFunctions
-} from '@angular/fire/functions';
-import {
-  getMessaging,
-  provideMessaging
-} from '@angular/fire/messaging';
-import {
-  getPerformance,
-  providePerformance
-} from '@angular/fire/performance';
-import {
-  getStorage,
-  provideStorage
-} from '@angular/fire/storage';
-import {
-  getRemoteConfig,
-  provideRemoteConfig
-} from '@angular/fire/remote-config';
-import {
-  getVertexAI,
-  provideVertexAI
-} from '@angular/fire/vertexai-preview';
-import {
   environment
 } from '../environments/environment';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideAnalytics(() => getAnalytics()),
-    ScreenTrackingService,
-    UserTrackingService,
-    provideAppCheck(() => {
-      return initializeAppCheck(undefined, {
-        provider: new ReCaptchaV3Provider('6LdqRxkqAAAAAGMz68iASWKbF-WybGZhy7hj6aoS'),
-        isTokenAutoRefreshEnabled: true
-      });
-    }),
-    provideFirestore(() => getFirestore()),
-    provideDatabase(() => getDatabase()),
-    provideFunctions(() => getFunctions()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideStorage(() => getStorage()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideVertexAI(() => getVertexAI())
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
   ]
 };
