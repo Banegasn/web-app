8b9afccdac08c0d856c7b7ebe0cd7f57fb5427c5
banegasn <nicobanegas.sc@gmail.com>
2024-07-27 00:13:58 +0200

    zoneless and firebase angular fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDNXjh_Eq20qSFOJ-oH0Deneq3oval5LrM",
+        authDomain: "banegasn-ad424.firebaseapp.com",
+        projectId: "banegasn-ad424",
+        storageBucket: "banegasn-ad424.appspot.com",
+        messagingSenderId: "782689941214",
+        appId: "1:782689941214:web:4bba648de8acae5f05744f",
+        measurementId: "G-23P9VW7L5M"
+    }
+  };
