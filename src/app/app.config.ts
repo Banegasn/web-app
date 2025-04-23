27e8ec7e0057c048221536f5b76a0f97faf023a7
banegasn <nicobanegas.sc@gmail.com>
2025-04-23 14:49:02 +0200

    Add Google Tag Manager integration to index.html and clean up app.config.ts imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,9 @@
-import {
-  ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
-} from '@angular/core';
-import {
-  provideRouter
-} from '@angular/router';
-import {
-  routes
-} from './app.routes';
-import {
-  provideClientHydration
-} from '@angular/platform-browser';
-import {
-  initializeApp,
-  provideFirebaseApp
-} from '@angular/fire/app';
-import {
-  environment
-} from '../environments/environment';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
 
 
 export const appConfig: ApplicationConfig = {
