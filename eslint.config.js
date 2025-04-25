e38538c434cc271b824d6415ca176810b93b55b9
banegasn <nicobanegas.sc@gmail.com>
2025-04-25 13:31:02 +0200

    Update ESLint configuration to ignore 'dist' and 'node_modules' directories, improving linting efficiency. Remove commented-out API endpoint code in server.ts for cleaner codebase.

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ const angular = require("angular-eslint");
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
+    ignores: ["**/dist/**", "**/node_modules/**"],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -36,6 +37,7 @@ module.exports = tseslint.config(
   },
   {
     files: ["**/*.html"],
+    ignores: ["**/dist/**", "**/node_modules/**"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
